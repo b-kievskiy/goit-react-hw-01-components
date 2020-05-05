2296e1ee02e4f6535d1d17912d387605fdd6ca5a
Bogdan <mormul96@ukr.net>
2020-05-05 17:15:43 +0300

    Updates

---
+++ precache-manifest.a42f1e360e8fcf39d71c430c5d7f8684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482820df8d3617e8bf6a4b98fbfa08c3",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "7f32ab9675132f3d2377",
+    "url": "/goit-react-hw-01-components/static/css/main.ba22234a.chunk.css"
+  },
+  {
+    "revision": "d02e543e0470e178349c",
+    "url": "/goit-react-hw-01-components/static/js/2.18da61ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.18da61ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f32ab9675132f3d2377",
+    "url": "/goit-react-hw-01-components/static/js/main.2a4d59fa.chunk.js"
+  },
+  {
+    "revision": "5a18dd9396dd6a18bba5",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.702437d4.js"
+  }
+]);
